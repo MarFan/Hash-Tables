4f8343bd669180c7a3d1e6a771562348ca6335a7
Jonah Aitchison <jonah@pop-os.localdomain>
2020-05-05 10:27:56 -0500

    reverted back

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -58,20 +58,20 @@ class HashTable:
 
         Implement this.
         """
-        # index = self.hash_index(key)      
-        # self.storage[index] = HashTableEntry(key, value)
+        index = self.hash_index(key)      
+        self.storage[index] = HashTableEntry(key, value)
 
-        index = self.hash_index(key)
-        if self.storage[index] is not None:
-            for n in self.storage[index]:
-                if n[0] == key:
-                    n[1] = value
-            else:
-                self.storage[index].append([key, value])
+        # index = self.hash_index(key)
+        # if self.storage[index] is not None:
+        #     for n in self.storage[index]:
+        #         if n[0] == key:
+        #             n[1] = value
+        #     else:
+        #         self.storage[index].append([key, value])
             
-        else:
-            self.storage[index] = []
-            self.storage[index].append([key, value])
+        # else:
+        #     self.storage[index] = []
+        #     self.storage[index].append([key, value])
 
 
     def delete(self, key):
@@ -97,13 +97,17 @@ class HashTable:
         Implement this.
         """
         index = self.hash_index(key)
-        if self.storage[index] is None:
-            return None
-        else:
-            for n in self.storage[index]:
-                if n[0] == key:
-                    return n[1]
-            return None
+        return self.storage[index].value
+        
+        # if self.storage[index] is None:
+        #     return None
+        # else:
+        #     for n in self.storage[index]:
+        #         if n[0] == key:
+        #             return n[1]
+        #     return None
+
+
         # if self.storage[index] is not None:
         #     for kvp in self.storage[index]:
         #         if kvp[0] == key:
